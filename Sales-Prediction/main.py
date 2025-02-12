e931220306ce69b6f50c128b28a023cf232549a5
Hannah Johnson <hannahjohnson2@westernwyoming.edu>
2025-02-12 22:29:48 +0000

    ready for grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,19 @@
 #
-# Name
-# Date
+# Hannah Johnson
+# 02/12/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
+# totalSales
+# profit
 
 # Get the amount of projected sales.
+totalSales = float(input('Enter the projected sales: ')) # Reads the input
 
 # Calculate the projected profit.
+profit = totalSales * 0.23 # totalSales * 0.23 (23%)
 
 # Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
