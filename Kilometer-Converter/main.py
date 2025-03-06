b76ca8edc7291e4287826c9ea651a4c0332cac3b
Hannah Johnson <hannahjohnson2@westernwyoming.edu>
2025-03-06 01:02:24 +0000

    Ready for grading

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Hannah Johnson
+# 03/5/2025
 # Kilometer Converter Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Variables
+distanceInKilo = 0
+distanceInMiles = 0
+CONVERSION_FORMULA = 0.6214 # Use a constant to hold this number
+
+# Asks the user to enter a distance in kilometers
+def main():
+    distanceInKilo = float(input('Enter a distance in kilometers: '))
+
+    # Call distanceToMiles function to display distance to miles
+    distanceToMiles(distanceInKilo)
+
+# Convert distance to miles
+def distanceToMiles(kilometers):
+
+    # Formula to convert km to miles
+    distanceInMiles = kilometers * CONVERSION_FORMULA
+
+    # Displays distance to miles
+    # Only displays 2 decimal points
+    print(f'{kilometers} kilometers equals {distanceInMiles: .3f} miles.')
+
+# Call main function to start/execute the program
+main()
